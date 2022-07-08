474bfab57f13a1d3a0aab673a6e282eed795083d
Marina <marinadlqs@gmail.com>
2022-07-08 17:31:12 +0200

    Install Angular project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Marina_Navarrete_Front-Final-Project-202205-MAD';
+}
