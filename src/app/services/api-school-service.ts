16fc33c6a86a2cb3489cf7c76a0014cd7b335077
Marina <marinadlqs@gmail.com>
2022-07-11 15:14:42 +0200

    Add school api service

---
+++ src/app/services/api-school-service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { iSchool } from '../models/school-model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  apiUrl: string;
+  constructor(private http: HttpClient) {
+    this.apiUrl = 'http://localhost:3453/school/';
+  }
+
+  getSchools(): Observable<Array<iSchool>> {
+    return this.http.get(this.apiUrl) as Observable<Array<iSchool>>;
+  }
+
+  getSchool(id: iSchool['_id']): Observable<iSchool> {
+    return this.http.get(this.apiUrl + id) as Observable<iSchool>;
+  }
+}
