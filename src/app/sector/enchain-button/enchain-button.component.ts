1140f9140eb6acec15dbb30433dd09516844ddf5
Marina <marinadlqs@gmail.com>
2022-07-11 12:49:47 +0200

    Add sector module and its components

---
+++ src/app/sector/enchain-button/enchain-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-enchain-button',
+  templateUrl: './enchain-button.component.html',
+  styleUrls: ['./enchain-button.component.scss']
+})
+export class EnchainButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
