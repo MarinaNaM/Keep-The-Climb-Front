79be3990f113dc81ee293c5a1a4f488edcc48d45
Marina <marinadlqs@gmail.com>
2022-07-11 12:25:05 +0200

    Add sector-one module with its components

---
+++ src/app/school-one/info-button/info-button.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InfoButtonComponent } from './info-button.component';
+
+describe('InfoButtonComponent', () => {
+  let component: InfoButtonComponent;
+  let fixture: ComponentFixture<InfoButtonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ InfoButtonComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(InfoButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
