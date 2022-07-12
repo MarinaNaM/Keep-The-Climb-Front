ccf44b0b0ed2c534f9fdba468745f3492a35eab4
Marina <marinadlqs@gmail.com>
2022-07-12 16:19:26 +0200

    Update sign in button component

--- src/app/landing/sign-in-button/sign-in-button.component.ts
+++ src/app/landing/sign-in-button/sign-in-button.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in-button',
   templateUrl: './sign-in-button.component.html',
-  styleUrls: ['./sign-in-button.component.scss']
+  styleUrls: ['./sign-in-button.component.scss'],
 })
 export class SignInButtonComponent implements OnInit {
+  constructor(public router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  goSignIn() {
+    this.router.navigate(['SignIn']);
   }
-
 }
