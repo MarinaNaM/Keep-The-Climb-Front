fde6ea4b92edb6e4c9e470cdeaffed9a193cf307
Marina <marinadlqs@gmail.com>
2022-07-12 20:20:39 +0200

    Update sign in component

--- src/app/sign-in/sign-in-form/sign-in-form.component.ts
+++ src/app/sign-in/sign-in-form/sign-in-form.component.ts
@@ -1,15 +1,83 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { iUser } from 'src/app/core/models/user-model';
+import { ApiUsersService } from 'src/app/core/services/api-users-service';
+import { LocalStorageService } from 'src/app/core/services/local-storage-service';
+import { loadUser } from 'src/app/store/actions/user-actions';
+import { AppState } from 'src/app/store/app.state';
 
 @Component({
   selector: 'app-sign-in-form',
   templateUrl: './sign-in-form.component.html',
-  styleUrls: ['./sign-in-form.component.scss']
+  styleUrls: ['./sign-in-form.component.scss'],
 })
 export class SignInFormComponent implements OnInit {
-
-  constructor() { }
+  registerData!: {
+    name: string;
+    email: string;
+    psw: string;
+    rPsw: string;
+  };
+  passwordError!: boolean;
+  emailError!: boolean;
+  constructor(
+    public userApi: ApiUsersService,
+    public store: Store<AppState>,
+    public router: Router,
+    public localStorage: LocalStorageService
+  ) {}
 
   ngOnInit(): void {
+    this.registerData = {
+      name: '',
+      email: '',
+      psw: '',
+      rPsw: '',
+    };
+    this.passwordError = false;
+    this.emailError = false;
   }
 
+  handleSubmit() {
+    if (
+      this.registerData.name &&
+      this.registerData.email &&
+      this.registerData.psw &&
+      this.registerData.rPsw
+    ) {
+      if (this.registerData.psw === this.registerData.rPsw) {
+        this.passwordError = false;
+        const newUser: iUser = {
+          name: '',
+          psw: '',
+          img: '',
+          email: '',
+          address: {
+            community: '',
+            province: '',
+          },
+
+          routes: [{ route: '', isProject: false, isEnchain: false }],
+        };
+        this.userApi.addUser(newUser).subscribe({
+          next: (data) => {
+            if (data.token) {
+              this.store.dispatch(
+                loadUser({ user: data.user, token: data.token })
+              );
+              this.localStorage.saveToken(data.token);
+              this.router.navigate(['home']);
+            }
+          },
+          error: (err) => {
+            this.emailError = true;
+            this.registerData.email = '';
+          },
+        });
+      } else {
+        this.passwordError = true;
+      }
+    }
+  }
 }
--- src/app/sign-in/sign-in.module.ts
+++ src/app/sign-in/sign-in.module.ts
@@ -1,21 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { SignInRoutingModule } from './sign-in-routing.module';
 import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
 import { SignInFormComponent } from './sign-in-form/sign-in-form.component';
-import { LoginButtonComponent } from './login-button/login-button.component';
-
 
 @NgModule({
-  declarations: [
-    SignInPageComponent,
-    SignInFormComponent,
-    LoginButtonComponent
-  ],
-  imports: [
-    CommonModule,
-    SignInRoutingModule
-  ]
+  declarations: [SignInPageComponent, SignInFormComponent],
+  imports: [CommonModule, SignInRoutingModule],
 })
-export class SignInModule { }
+export class SignInModule {}
