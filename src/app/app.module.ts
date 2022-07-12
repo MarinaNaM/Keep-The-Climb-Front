835274ea4039c0e29aa189a3e20c577849a51d7e
Marina <marinadlqs@gmail.com>
2022-07-12 13:42:21 +0200

    Add test call api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './schools/home-page/home-page.component';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { ROOT_REDUCERS } from './store/app.state';
+import { environment } from 'src/environments/environment';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent],
+  declarations: [AppComponent],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot(ROOT_REDUCERS),
-    StoreDevtoolsModule.instrument({ name: 'TEST' }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
