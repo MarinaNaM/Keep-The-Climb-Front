4d2c7ec2d5af9a4545e6d36cdf1c65befe0313ce
Marina <marinadlqs@gmail.com>
2022-07-12 11:34:03 +0200

    Add sector reducer

---
+++ src/app/store/reducers/sector-reducer.ts
@@ -0,0 +1,14 @@
+import { createReducer, on } from '@ngrx/store';
+import { iSectorState } from 'src/app/core/models/sector-model';
+import { loadSectorList } from '../actions/sector-actions';
+
+export const initialState: iSectorState = {
+  sectors: [],
+};
+
+export const sectorReducer = createReducer(
+  initialState,
+  on(loadSectorList, (state, { sectors }) => {
+    return { sectors: [...sectors] };
+  })
+);
