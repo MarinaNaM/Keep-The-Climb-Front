69b95bfa068874802c1206d51a795c29fd686e40
Marina <marinadlqs@gmail.com>
2022-07-12 16:18:53 +0200

    Update without login component

--- src/app/landing/without-login-button/without-login-button.component.ts
+++ src/app/landing/without-login-button/without-login-button.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-without-login-button',
   templateUrl: './without-login-button.component.html',
-  styleUrls: ['./without-login-button.component.scss']
+  styleUrls: ['./without-login-button.component.scss'],
 })
 export class WithoutLoginButtonComponent implements OnInit {
+  constructor(public router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  goHome() {
+    this.router.navigate(['home']);
   }
-
 }
