af8123cfe22e51061d660a2c5962c4f734e5a6b8
Marina <marinadlqs@gmail.com>
2022-07-13 14:47:49 +0200

    Add without login button component test

--- src/app/landing/without-login-button/without-login-button.component.spec.ts
+++ src/app/landing/without-login-button/without-login-button.component.spec.ts
@@ -1,16 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { provideMockStore } from '@ngrx/store/testing';
+import { AppState } from 'src/app/store/app.state';
 import { WithoutLoginButtonComponent } from './without-login-button.component';
+
+const mockInitialState: AppState = {
+  schools: { schools: [] },
+  sectors: { sectors: [] },
+  routes: { routes: [] },
+  user: {
+    user: {
+      _id: '123456',
+      name: 'test-user',
+      psw: '123456789',
+      img: 'src',
+      email: 'usertest@gmail.com',
+      address: {
+        community: '',
+        province: '',
+      },
+      role: 'user',
+      routes: [
+        {
+          route: '123456789012345678901234',
+          isProject: false,
+          isEnchain: false,
+        },
+      ],
+    },
+    token: '',
+  },
+};
 
 describe('WithoutLoginButtonComponent', () => {
   let component: WithoutLoginButtonComponent;
   let fixture: ComponentFixture<WithoutLoginButtonComponent>;
+  let initialState = mockInitialState;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [WithoutLoginButtonComponent],
       imports: [RouterTestingModule],
+      providers: [provideMockStore({ initialState })],
     }).compileComponents();
 
     fixture = TestBed.createComponent(WithoutLoginButtonComponent);
