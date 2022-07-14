f26283e3ebfacba1b808503f3c7a52833607fab3
Marina <marinadlqs@gmail.com>
2022-07-14 11:18:40 +0200

    Update home card component

--- src/app/home/schools-card/schools-card.component.ts
+++ src/app/home/schools-card/schools-card.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { iSchool, iSchoolState } from 'src/app/core/models/school-model';
 
 @Component({
   selector: 'app-schools-card',
   templateUrl: './schools-card.component.html',
-  styleUrls: ['./schools-card.component.scss']
+  styleUrls: ['./schools-card.component.scss'],
 })
 export class SchoolsCardComponent implements OnInit {
-
-  constructor() { }
+  schools!: ReadonlyArray<iSchool>;
+  constructor(public store: Store<{ schools: iSchoolState }>) {}
 
   ngOnInit(): void {
+    this.store
+      .select((state) => state.schools)
+      .subscribe({
+        next: (data) => {
+          console.log(data);
+          this.schools = data.schools;
+        },
+      });
   }
-
 }
