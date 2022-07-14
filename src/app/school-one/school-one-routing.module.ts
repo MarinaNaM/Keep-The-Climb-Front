2a5d51cdcceba3c66395c18b40e91dd55ce172b0
Marina <marinadlqs@gmail.com>
2022-07-14 14:12:47 +0200

    Update routing in chool-one module

--- src/app/school-one/school-one-routing.module.ts
+++ src/app/school-one/school-one-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SchoolPageComponent } from './school-page/school-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: SchoolPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class SchoolOneRoutingModule { }
+export class SchoolOneRoutingModule {}
--- src/app/school-one/school-one.module.ts
+++ src/app/school-one/school-one.module.ts
@@ -5,17 +5,11 @@ import { SchoolOneRoutingModule } from './school-one-routing.module';
 import { SchoolPageComponent } from './school-page/school-page.component';
 import { SectorsComponent } from './sectors/sectors.component';
 import { InfoButtonComponent } from './info-button/info-button.component';
-
+import { NavbarModule } from '../navbar/navbar.module';
+import { SectorCardComponent } from './sector-card/sector-card.component';
 
 @NgModule({
-  declarations: [
-    SchoolPageComponent,
-    SectorsComponent,
-    InfoButtonComponent
-  ],
-  imports: [
-    CommonModule,
-    SchoolOneRoutingModule
-  ]
+  declarations: [SchoolPageComponent, SectorsComponent, InfoButtonComponent, SectorCardComponent],
+  imports: [CommonModule, SchoolOneRoutingModule, NavbarModule],
 })
-export class SchoolOneModule { }
+export class SchoolOneModule {}
