95b7b5f09de56a59980cffa134553572b1150271
Marina <marinadlqs@gmail.com>
2022-07-14 14:11:12 +0200

    Update sector lis component

--- src/app/school-one/sectors/sectors.component.ts
+++ src/app/school-one/sectors/sectors.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { iSchool } from 'src/app/core/models/school-model';
+import { AppState } from 'src/app/store/app.state';
 @Component({
   selector: 'app-sectors',
   templateUrl: './sectors.component.html',
-  styleUrls: ['./sectors.component.scss']
+  styleUrls: ['./sectors.component.scss'],
 })
 export class SectorsComponent implements OnInit {
-
-  constructor() { }
+  idSchool = this.route.snapshot.paramMap.get('id') as string;
+  school!: iSchool | undefined;
+  constructor(public route: ActivatedRoute, public store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.store
+      .select((state) => state.schools)
+      .subscribe({
+        next: (data) => {
+          this.school = data.schools.find((item) => item._id === this.idSchool);
+          console.log(this.school);
+        },
+      });
   }
-
 }
