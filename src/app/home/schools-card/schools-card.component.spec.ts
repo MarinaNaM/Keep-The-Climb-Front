cbae3ab79d63838c0cd15a014efac4412aeb54ee
Marina <marinadlqs@gmail.com>
2022-07-14 17:09:48 +0200

    Update school card component

--- src/app/home/schools-card/schools-card.component.spec.ts
+++ src/app/home/schools-card/schools-card.component.spec.ts
@@ -1,16 +1,50 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { AppState } from 'src/app/store/app.state';
 
 import { SchoolsCardComponent } from './schools-card.component';
+
+const mockInitialState: AppState = {
+  schools: { schools: [] },
+  sectors: { sectors: [] },
+  routes: { routes: [] },
+  user: {
+    user: {
+      _id: '123456',
+      name: 'test-user',
+      psw: '123456789',
+      img: 'src',
+      email: 'usertest@gmail.com',
+      address: {
+        community: '',
+        province: '',
+      },
+      role: 'user',
+      routes: [
+        {
+          route: '123456789012345678901234',
+          isProject: false,
+          isEnchain: false,
+        },
+      ],
+    },
+    token: '',
+  },
+};
 
 describe('SchoolsCardComponent', () => {
   let component: SchoolsCardComponent;
   let fixture: ComponentFixture<SchoolsCardComponent>;
+  let initialState = mockInitialState;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SchoolsCardComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [SchoolsCardComponent],
+      providers: [provideMockStore({ initialState })],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SchoolsCardComponent);
     component = fixture.componentInstance;
