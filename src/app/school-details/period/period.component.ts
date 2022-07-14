c0bf5be10484ac07f1d01ff179ece61600d4d91c
Marina <marinadlqs@gmail.com>
2022-07-14 19:19:06 +0200

    Update component period

--- src/app/school-details/period/period.component.ts
+++ src/app/school-details/period/period.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { iSchool } from 'src/app/core/models/school-model';
+import { AppState } from 'src/app/store/app.state';
 
 @Component({
   selector: 'app-period',
   templateUrl: './period.component.html',
-  styleUrls: ['./period.component.scss']
+  styleUrls: ['./period.component.scss'],
 })
 export class PeriodComponent implements OnInit {
+  school!: iSchool | undefined;
+  idSchool = this.route.snapshot.paramMap.get('id') as string;
 
-  constructor() { }
+  constructor(public route: ActivatedRoute, public store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.store
+      .select((state) => state.schools)
+      .subscribe({
+        next: (data) => {
+          this.school = data.schools.find((item) => item._id === this.idSchool);
+        },
+      });
   }
-
 }
