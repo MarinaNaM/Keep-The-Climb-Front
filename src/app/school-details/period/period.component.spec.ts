82a2654cf402c3f88f1541cb93253e544e65ccb7
Marina <marinadlqs@gmail.com>
2022-07-14 19:39:20 +0200

    Update period component test

--- src/app/school-details/period/period.component.spec.ts
+++ src/app/school-details/period/period.component.spec.ts
@@ -1,16 +1,78 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { iSchool } from 'src/app/core/models/school-model';
+import { iSector } from 'src/app/core/models/sector-model';
+import { AppState } from 'src/app/store/app.state';
 
 import { PeriodComponent } from './period.component';
+
+const mockSchool: iSchool = {
+  _id: '123',
+  name: 'school-test',
+  img: 'src',
+  period: 'mañana',
+  localization: {
+    lat: 1,
+    lng: 2,
+  },
+  sectors: [{} as iSector],
+};
+
+const mockInitialState: AppState = {
+  schools: { schools: [mockSchool] },
+  sectors: { sectors: [] },
+  routes: { routes: [] },
+  user: {
+    user: {
+      _id: '123456',
+      name: 'test-user',
+      psw: '123456789',
+      img: 'src',
+      email: 'usertest@gmail.com',
+      address: {
+        community: '',
+        province: '',
+      },
+      role: 'user',
+      routes: [
+        {
+          route: '123456789012345678901234',
+          isProject: false,
+          isEnchain: false,
+        },
+      ],
+    },
+    token: '',
+  },
+};
 
 describe('PeriodComponent', () => {
   let component: PeriodComponent;
   let fixture: ComponentFixture<PeriodComponent>;
+  let initialState = mockInitialState;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PeriodComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [PeriodComponent],
+      providers: [
+        PeriodComponent,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: convertToParamMap({
+                id: '1',
+              }),
+            },
+          },
+        },
+        provideMockStore({ initialState }),
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PeriodComponent);
     component = fixture.componentInstance;
