7be051ba915b080495c7a5a76ba9b8660061a249
Marina <marinadlqs@gmail.com>
2022-07-15 15:57:40 +0200

    Update test router list component

--- src/app/sector/routes-list/routes-list.component.spec.ts
+++ src/app/sector/routes-list/routes-list.component.spec.ts
@@ -1,16 +1,77 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { iSector } from 'src/app/core/models/sector-model';
+import { AppState } from 'src/app/store/app.state';
 
 import { RoutesListComponent } from './routes-list.component';
+
+const mockSector: iSector = {
+  _id: '123',
+  name: '',
+  img: '',
+  hoursSun: 'mañana',
+  localization: {
+    lat: 0,
+    lng: 0,
+  },
+  routes: [],
+};
+
+const mockInitialState: AppState = {
+  schools: { schools: [] },
+  sectors: { sectors: [mockSector] },
+  routes: { routes: [] },
+  user: {
+    user: {
+      _id: '123456',
+      name: 'test-user',
+      psw: '123456789',
+      img: 'src',
+      email: 'usertest@gmail.com',
+      address: {
+        community: '',
+        province: '',
+      },
+      role: 'user',
+      routes: [
+        {
+          route: '123456789012345678901234',
+          isProject: false,
+          isEnchain: false,
+        },
+      ],
+    },
+    token: '',
+  },
+};
 
 describe('RoutesListComponent', () => {
   let component: RoutesListComponent;
   let fixture: ComponentFixture<RoutesListComponent>;
+  let initialState = mockInitialState;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RoutesListComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [RoutesListComponent],
+      providers: [
+        RoutesListComponent,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: convertToParamMap({
+                id: '1',
+              }),
+            },
+          },
+        },
+        provideMockStore({ initialState }),
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RoutesListComponent);
     component = fixture.componentInstance;
