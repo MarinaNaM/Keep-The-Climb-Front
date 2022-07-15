e65ebbf46f8b9fc6f9c0fa731da78919b9145789
Marina <marinadlqs@gmail.com>
2022-07-15 12:12:37 +0200

    Delete component in sector module

--- src/app/sector/sector.module.ts
+++ src/app/sector/sector.module.ts
@@ -7,7 +7,6 @@ import { RoutesListComponent } from './routes-list/routes-list.component';
 import { RoutesCardComponent } from './routes-card/routes-card.component';
 import { EnchainButtonComponent } from './enchain-button/enchain-button.component';
 import { ProjectButtonComponent } from './project-button/project-button.component';
-import { VoteButtonComponent } from './vote-button/vote-button.component';
 import { ApproachButtonComponent } from './approach-button/approach-button.component';
 import { NavbarModule } from '../navbar/navbar.module';
 
@@ -18,7 +17,6 @@ import { NavbarModule } from '../navbar/navbar.module';
     RoutesCardComponent,
     EnchainButtonComponent,
     ProjectButtonComponent,
-    VoteButtonComponent,
     ApproachButtonComponent,
   ],
   imports: [CommonModule, SectorRoutingModule, NavbarModule],
