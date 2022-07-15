4be689d30320408e49c2fac97e7c0746bf3f6317
Marina <marinadlqs@gmail.com>
2022-07-15 15:33:22 +0200

    Update sector page with its components

--- src/app/sector/sector-page/sector-page.component.ts
+++ src/app/sector/sector-page/sector-page.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { iSchool } from 'src/app/core/models/school-model';
+import { iSector } from 'src/app/core/models/sector-model';
+import { AppState } from 'src/app/store/app.state';
 
 @Component({
   selector: 'app-sector-page',
   templateUrl: './sector-page.component.html',
-  styleUrls: ['./sector-page.component.scss']
+  styleUrls: ['./sector-page.component.scss'],
 })
 export class SectorPageComponent implements OnInit {
-
-  constructor() { }
+  sector!: iSector | undefined;
+  idSector!: string;
+  constructor(public route: ActivatedRoute, public store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.idSector = this.route.snapshot.paramMap.get('id') as string;
+    this.store
+      .select((state) => state.sectors)
+      .subscribe({
+        next: (data) => {
+          this.sector = data.sectors.find((item) => item._id === this.idSector);
+        },
+      });
   }
-
 }
