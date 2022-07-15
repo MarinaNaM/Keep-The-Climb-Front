6a29dfeb9b9476baa8d4911d4c115cdb82152b53
Marina <marinadlqs@gmail.com>
2022-07-15 18:42:28 +0200

    Update enchains list component

--- src/app/enchains/enchains-list/enchains-list.component.ts
+++ src/app/enchains/enchains-list/enchains-list.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { iRoute } from 'src/app/core/models/route-model';
+import { AppState } from 'src/app/store/app.state';
 
 @Component({
   selector: 'app-enchains-list',
   templateUrl: './enchains-list.component.html',
-  styleUrls: ['./enchains-list.component.scss']
+  styleUrls: ['./enchains-list.component.scss'],
 })
 export class EnchainsListComponent implements OnInit {
-
-  constructor() { }
+  enchainedRoutes!: Array<{
+    route: iRoute;
+    isProject: boolean;
+    isEnchain: boolean;
+  }>;
+  constructor(public store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.store
+      .select((state) => state.user)
+      .subscribe({
+        next: (data) => {
+          this.enchainedRoutes = data.user.routes.filter(
+            (item) => item.isEnchain === true
+          );
+          console.log(data);
+        },
+      });
   }
-
 }
