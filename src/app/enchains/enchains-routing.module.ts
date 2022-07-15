3bffabfbb606e529bbd1562d938b4df9740945bc
Marina <marinadlqs@gmail.com>
2022-07-15 18:40:51 +0200

    Add routing in enchains module

--- src/app/enchains/enchains-routing.module.ts
+++ src/app/enchains/enchains-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EnchainPageComponent } from './enchain-page/enchain-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: EnchainPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class EnchainsRoutingModule { }
+export class EnchainsRoutingModule {}
