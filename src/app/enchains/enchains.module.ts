85401d0ac3380b7a663c0f93779b321821a6e17d
Marina <marinadlqs@gmail.com>
2022-07-15 18:41:19 +0200

    Import navbar in enchains module

--- src/app/enchains/enchains.module.ts
+++ src/app/enchains/enchains.module.ts
@@ -4,16 +4,10 @@ import { CommonModule } from '@angular/common';
 import { EnchainsRoutingModule } from './enchains-routing.module';
 import { EnchainPageComponent } from './enchain-page/enchain-page.component';
 import { EnchainsListComponent } from './enchains-list/enchains-list.component';
-
+import { NavbarModule } from '../navbar/navbar.module';
 
 @NgModule({
-  declarations: [
-    EnchainPageComponent,
-    EnchainsListComponent
-  ],
-  imports: [
-    CommonModule,
-    EnchainsRoutingModule
-  ]
+  declarations: [EnchainPageComponent, EnchainsListComponent],
+  imports: [CommonModule, EnchainsRoutingModule, NavbarModule],
 })
-export class EnchainsModule { }
+export class EnchainsModule {}
