f93e581cada12081538fc1d6494a831bf6e84bed
Marina <marinadlqs@gmail.com>
2022-07-16 13:23:40 +0200

    Add edit button component

--- src/app/enchains/enchains.module.ts
+++ src/app/enchains/enchains.module.ts
@@ -5,9 +5,10 @@ import { EnchainsRoutingModule } from './enchains-routing.module';
 import { EnchainPageComponent } from './enchain-page/enchain-page.component';
 import { EnchainsListComponent } from './enchains-list/enchains-list.component';
 import { NavbarModule } from '../navbar/navbar.module';
+import { EditButtonComponent } from './edit-button/edit-button.component';
 
 @NgModule({
-  declarations: [EnchainPageComponent, EnchainsListComponent],
+  declarations: [EnchainPageComponent, EnchainsListComponent, EditButtonComponent],
   imports: [CommonModule, EnchainsRoutingModule, NavbarModule],
 })
 export class EnchainsModule {}
