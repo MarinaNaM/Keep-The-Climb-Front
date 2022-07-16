02f46bb12fba17362cc6ec0e9ef2f6fff1d37db8
Marina <marinadlqs@gmail.com>
2022-07-16 18:49:45 +0200

    Add routing and lazyloading

--- src/app/vote/vote-routing.module.ts
+++ src/app/vote/vote-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VotePageComponent } from './vote-page/vote-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: VotePageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class VoteRoutingModule { }
+export class VoteRoutingModule {}
