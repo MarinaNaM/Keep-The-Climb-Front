91e8894df99d6b41445026a07e19c3bc72041253
Marina <marinadlqs@gmail.com>
2022-07-16 14:59:16 +0200

    Create new component in projects module

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsPageComponent } from './projects-page/projects-page.component';
-import { RouteCardComponent } from './route-card/route-card.component';
+
 import { NavbarModule } from '../navbar/navbar.module';
 
 @NgModule({
-  declarations: [ProjectsPageComponent, RouteCardComponent],
+  declarations: [ProjectsPageComponent],
   imports: [CommonModule, ProjectsRoutingModule, NavbarModule],
 })
 export class ProjectsModule {}
