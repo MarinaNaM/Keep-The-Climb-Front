2d22f625062b4056dd45a465f0576dc2639a30a7
Marina <marinadlqs@gmail.com>
2022-07-16 17:57:03 +0200

    Update projects list component

--- src/app/projects/projects-list/projects-list.component.ts
+++ src/app/projects/projects-list/projects-list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { iRoute } from 'src/app/core/models/route-model';
+import { AppState } from 'src/app/store/app.state';
 
 @Component({
   selector: 'app-projects-list',
   templateUrl: './projects-list.component.html',
-  styleUrls: ['./projects-list.component.scss']
+  styleUrls: ['./projects-list.component.scss'],
 })
 export class ProjectsListComponent implements OnInit {
-
-  constructor() { }
+  projectsRoutes!: Array<{
+    route: iRoute;
+    isProject: boolean;
+    isEnchain: boolean;
+  }>;
+  constructor(public store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.store
+      .select((state) => state.user)
+      .subscribe({
+        next: (data) => {
+          this.projectsRoutes = data.user.routes.filter(
+            (item) => item.isProject === true
+          );
+        },
+      });
   }
-
 }
