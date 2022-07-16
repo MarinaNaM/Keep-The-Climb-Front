5449808ad5ae6cc45fdac420e758a37b0c85b41d
Marina <marinadlqs@gmail.com>
2022-07-16 17:56:00 +0200

    Create project lis and enchain button components

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -5,9 +5,11 @@ import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsPageComponent } from './projects-page/projects-page.component';
 
 import { NavbarModule } from '../navbar/navbar.module';
+import { ProjectsListComponent } from './projects-list/projects-list.component';
+import { EnchainButtonComponent } from './enchain-button/enchain-button.component';
 
 @NgModule({
-  declarations: [ProjectsPageComponent],
+  declarations: [ProjectsPageComponent, ProjectsListComponent, EnchainButtonComponent],
   imports: [CommonModule, ProjectsRoutingModule, NavbarModule],
 })
 export class ProjectsModule {}
