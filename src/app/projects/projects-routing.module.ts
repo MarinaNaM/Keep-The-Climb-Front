ab0daa3aebffac5ab0b4fc319d62c600bd5004db
Marina <marinadlqs@gmail.com>
2022-07-16 14:37:34 +0200

    Add routing of projects page

--- src/app/projects/projects-routing.module.ts
+++ src/app/projects/projects-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: ProjectsPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ProjectsRoutingModule { }
+export class ProjectsRoutingModule {}
