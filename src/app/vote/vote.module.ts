3da947c5ac3eb6b37eafa03fae0e8169a7c129b9
Marina <marinadlqs@gmail.com>
2022-07-16 18:55:09 +0200

    Add navbarModule in ngModule imorts

--- src/app/vote/vote.module.ts
+++ src/app/vote/vote.module.ts
@@ -5,17 +5,10 @@ import { VoteRoutingModule } from './vote-routing.module';
 import { VotePageComponent } from './vote-page/vote-page.component';
 import { VoteFormComponent } from './vote-form/vote-form.component';
 import { RouteCardComponent } from './route-card/route-card.component';
-
+import { NavbarModule } from '../navbar/navbar.module';
 
 @NgModule({
-  declarations: [
-    VotePageComponent,
-    VoteFormComponent,
-    RouteCardComponent
-  ],
-  imports: [
-    CommonModule,
-    VoteRoutingModule
-  ]
+  declarations: [VotePageComponent, VoteFormComponent, RouteCardComponent],
+  imports: [CommonModule, VoteRoutingModule, NavbarModule],
 })
-export class VoteModule { }
+export class VoteModule {}
