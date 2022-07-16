22e000f62166ba3a9991d8abab18044a72845710
Marina <marinadlqs@gmail.com>
2022-07-16 14:38:47 +0200

    Import navbar module in projects module

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -4,16 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsPageComponent } from './projects-page/projects-page.component';
 import { RouteCardComponent } from './route-card/route-card.component';
-
+import { NavbarModule } from '../navbar/navbar.module';
 
 @NgModule({
-  declarations: [
-    ProjectsPageComponent,
-    RouteCardComponent
-  ],
-  imports: [
-    CommonModule,
-    ProjectsRoutingModule
-  ]
+  declarations: [ProjectsPageComponent, RouteCardComponent],
+  imports: [CommonModule, ProjectsRoutingModule, NavbarModule],
 })
-export class ProjectsModule { }
+export class ProjectsModule {}
