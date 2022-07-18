772713c5888d79aca40d3c704ab686eac5e15578
Marina <marinadlqs@gmail.com>
2022-07-18 20:48:48 +0200

    Add profile page with its components

--- src/app/profile/profile-page/profile-page.component.ts
+++ src/app/profile/profile-page/profile-page.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { iUserState } from 'src/app/core/models/user-model';
+import { AppState } from 'src/app/store/app.state';
 
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
-  styleUrls: ['./profile-page.component.scss']
+  styleUrls: ['./profile-page.component.scss'],
 })
 export class ProfilePageComponent implements OnInit {
-
-  constructor() { }
+  user!: iUserState;
+  constructor(public store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.store
+      .select((state) => state.user)
+      .subscribe({
+        next: (data) => {
+          this.user = data;
+        },
+      });
   }
-
 }
