4031c9d7d091767eae6ca9bd95b8a406858ae0aa
Marina <marinadlqs@gmail.com>
2022-07-18 14:24:17 +0200

    Update test edit button component

--- src/app/enchains/edit-button/edit-button.component.spec.ts
+++ src/app/enchains/edit-button/edit-button.component.spec.ts
@@ -1,6 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { iRoute } from 'src/app/core/models/route-model';
 
 import { EditButtonComponent } from './edit-button.component';
+
+const mockRoute: iRoute = {
+  _id: '1',
+  name: '',
+  length: 0,
+  grade: '',
+  voteGrade: [],
+};
 
 describe('EditButtonComponent', () => {
   let component: EditButtonComponent;
@@ -8,12 +17,12 @@ describe('EditButtonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EditButtonComponent ]
-    })
-    .compileComponents();
+      declarations: [EditButtonComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(EditButtonComponent);
     component = fixture.componentInstance;
+    component.route = mockRoute;
     fixture.detectChanges();
   });
 
