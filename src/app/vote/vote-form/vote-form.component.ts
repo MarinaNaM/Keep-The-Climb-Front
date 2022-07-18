862aa1db92381af455aaf0e6935e7956e1411ea5
Marina <marinadlqs@gmail.com>
2022-07-18 20:53:52 +0200

    Update media users votes for update the user

--- src/app/vote/vote-form/vote-form.component.ts
+++ src/app/vote/vote-form/vote-form.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { gradeValues, valueToGrade } from 'src/app/core/helper/gradeValues';
+import { gradeValues, valueToGrade } from 'src/app/core/helpers/gradeValues';
 import { iRoute } from 'src/app/core/models/route-model';
 import { iUserState } from 'src/app/core/models/user-model';
 import { ApiRoutesService } from 'src/app/core/services/api-routes-service';
+import { ApiUsersService } from 'src/app/core/services/api-users-service';
 import { AppState } from 'src/app/store/app.state';
 import * as RouteActions from '../../store/actions/route-actions';
+import * as UserActions from '../../store/actions/user-actions';
 @Component({
   selector: 'app-vote-form',
   templateUrl: './vote-form.component.html',
@@ -22,7 +24,8 @@ export class VoteFormComponent implements OnInit {
   constructor(
     public store: Store<AppState>,
     public routeApi: ApiRoutesService,
-    public routeTo: Router
+    public routeTo: Router,
+    public userApi: ApiUsersService
   ) {}
 
   ngOnInit(): void {
@@ -64,6 +67,12 @@ export class VoteFormComponent implements OnInit {
           )
           .subscribe({
             next: (data) => {
+              this.userApi.loginUser(undefined, this.user.token).subscribe({
+                next: (data) => {
+                  this.user = data;
+                  this.store.dispatch(UserActions.loadUser(data));
+                },
+              });
               this.store.dispatch(
                 RouteActions.updateRoute({
                   id: this.route._id as string,
@@ -72,9 +81,50 @@ export class VoteFormComponent implements OnInit {
               );
             },
           });
+        // let updateUser = this.user.user.routes;
+        // const newRoutesUser = updateUser.map((item) =>
+        //   item.route._id === this.route._id
+        //     ? {
+        //         route: this.route,
+        //         isEnchain: item.isEnchain,
+        //         isProject: item.isProject,
+        //       }
+        //     : item
+        // );
+        // this.userApi
+        //   .updateUser(
+        //     this.user.user._id,
+        //     { routes: newRoutesUser },
+        //     this.user.token
+        //   )
+        //   .subscribe({
+        //     next: (data) => {
+        //       this.store.dispatch(
+        //         UserActions.loadUser({
+        //           user: this.user.user,
+        //           token: this.user.token,
+        //         })
+        // UserActions.updateUser({
+        //   data: {
+        //     user: { routes: newRoutesUser },
+        //   } as Partial<iUserState>,
+        // })
+        //     );
+        //   },
+        // });
       }
     } else {
-      this.routeTo.navigate(['login']);
+      this.routeTo.navigate(['']);
     }
   }
 }
+// let updateUser = this.user.user.routes;
+// const newRoutesUser = [...updateUser, this.route];
+
+// this.store.dispatch(
+//   UserActions.updateUser({
+//     data: {
+//       user: { routes: newRoutesUser },
+//     } as Partial<iUserState>,
+//   })
+// );
