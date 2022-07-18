f62688d0c3a86e924de5ab07b739f78d180f09e1
Marina <marinadlqs@gmail.com>
2022-07-18 13:29:13 +0200

    Update test route card component

--- src/app/vote/route-card/route-card.component.spec.ts
+++ src/app/vote/route-card/route-card.component.spec.ts
@@ -1,16 +1,76 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { provideMockStore } from '@ngrx/store/testing';
+import { iRoute } from 'src/app/core/models/route-model';
+import { AppState } from 'src/app/store/app.state';
 
 import { RouteCardComponent } from './route-card.component';
+
+const mockRoute: iRoute = {
+  _id: '1',
+  name: '',
+  length: 0,
+  grade: '',
+  voteGrade: [],
+};
+
+const mockInitialState: AppState = {
+  schools: { schools: [] },
+  sectors: { sectors: [] },
+  routes: { routes: [mockRoute] },
+  user: {
+    user: {
+      _id: '123456',
+      name: 'test-user',
+      psw: '123456789',
+      img: 'src',
+      email: 'usertest@gmail.com',
+      address: {
+        community: '',
+        province: '',
+      },
+      role: 'user',
+      routes: [
+        {
+          route: {
+            _id: '1',
+            name: '',
+            length: 1,
+            grade: '',
+            voteGrade: [{ user: '123456', vote: 1 }],
+          },
+          isProject: false,
+          isEnchain: true,
+        },
+      ],
+    },
+    token: '',
+  },
+};
 
 describe('RouteCardComponent', () => {
   let component: RouteCardComponent;
   let fixture: ComponentFixture<RouteCardComponent>;
+  let initialState = mockInitialState;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RouteCardComponent ]
-    })
-    .compileComponents();
+      declarations: [RouteCardComponent],
+      providers: [
+        RouteCardComponent,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: convertToParamMap({
+                id: '1',
+              }),
+            },
+          },
+        },
+        provideMockStore({ initialState }),
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RouteCardComponent);
     component = fixture.componentInstance;
