0a8a97ad13654b1c172ea01b86d45bcf7a53153a
Marina <marinadlqs@gmail.com>
2022-07-19 11:07:21 +0200

    Add test user data component

--- src/app/profile/user-data/user-data.component.spec.ts
+++ src/app/profile/user-data/user-data.component.spec.ts
@@ -1,6 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { iUser, iUserState } from 'src/app/core/models/user-model';
+import { AppState } from 'src/app/store/app.state';
 
 import { UserDataComponent } from './user-data.component';
+
+const mockUser: iUserState = {
+  user: {
+    name: '',
+    email: '',
+    psw: '',
+    address: {
+      community: '',
+      province: '',
+    },
+    routes: [],
+  },
+  token: '',
+};
 
 describe('UserDataComponent', () => {
   let component: UserDataComponent;
@@ -8,12 +25,12 @@ describe('UserDataComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserDataComponent ]
-    })
-    .compileComponents();
+      declarations: [UserDataComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(UserDataComponent);
     component = fixture.componentInstance;
+    component.user = mockUser;
     fixture.detectChanges();
   });
 
