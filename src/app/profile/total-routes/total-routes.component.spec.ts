f28417ef7bd0cd949bd65453a9bbe22fc81f1093
Marina <marinadlqs@gmail.com>
2022-07-19 10:23:10 +0200

    Add test total routes component

--- src/app/profile/total-routes/total-routes.component.spec.ts
+++ src/app/profile/total-routes/total-routes.component.spec.ts
@@ -1,16 +1,52 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { AppState } from 'src/app/store/app.state';
 
 import { TotalRoutesComponent } from './total-routes.component';
+
+const mockInitialState: AppState = {
+  schools: { schools: [] },
+  sectors: { sectors: [] },
+  routes: { routes: [] },
+  user: {
+    user: {
+      _id: '123456',
+      name: 'test-user',
+      psw: '123456789',
+      img: 'src',
+      email: 'usertest@gmail.com',
+      address: {
+        community: '',
+        province: '',
+      },
+      role: 'user',
+      routes: [
+        {
+          route: {
+            name: '',
+            length: 1,
+            grade: '',
+            voteGrade: [{ user: '', vote: 1 }],
+          },
+          isProject: false,
+          isEnchain: false,
+        },
+      ],
+    },
+    token: '',
+  },
+};
 
 describe('TotalRoutesComponent', () => {
   let component: TotalRoutesComponent;
   let fixture: ComponentFixture<TotalRoutesComponent>;
+  let initialState = mockInitialState;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TotalRoutesComponent ]
-    })
-    .compileComponents();
+      declarations: [TotalRoutesComponent],
+      providers: [provideMockStore({ initialState })],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TotalRoutesComponent);
     component = fixture.componentInstance;
