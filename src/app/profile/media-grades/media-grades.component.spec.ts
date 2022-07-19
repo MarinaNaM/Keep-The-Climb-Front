7a0fdfba101e5bbca6d1fe3e6942606899430b99
Marina <marinadlqs@gmail.com>
2022-07-19 13:13:48 +0200

    Add test media grades

--- src/app/profile/media-grades/media-grades.component.spec.ts
+++ src/app/profile/media-grades/media-grades.component.spec.ts
@@ -1,19 +1,58 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { AppState } from 'src/app/store/app.state';
 
 import { MediaGradesComponent } from './media-grades.component';
+
+const mockInitialState: AppState = {
+  schools: { schools: [] },
+  sectors: { sectors: [] },
+  routes: {
+    routes: [],
+  },
+  user: {
+    user: {
+      _id: '123456',
+      name: 'test-user',
+      psw: '123456789',
+      img: 'src',
+      email: 'usertest@gmail.com',
+      address: {
+        community: '',
+        province: '',
+      },
+      role: 'user',
+      routes: [
+        {
+          route: {
+            name: '',
+            length: 1,
+            grade: '6b',
+            voteGrade: [{ user: '', vote: 1 }],
+          },
+          isProject: false,
+          isEnchain: true,
+        },
+      ],
+    },
+    token: '',
+  },
+};
 
 describe('MediaGradesComponent', () => {
   let component: MediaGradesComponent;
   let fixture: ComponentFixture<MediaGradesComponent>;
+  let initialState = mockInitialState;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MediaGradesComponent ]
-    })
-    .compileComponents();
+      declarations: [MediaGradesComponent],
+      providers: [provideMockStore({ initialState })],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MediaGradesComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
