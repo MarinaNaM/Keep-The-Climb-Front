8c04b2e95160dbaa139fcd06e5920e7c0c99c6b0
Marina <marinadlqs@gmail.com>
2022-07-19 15:55:02 +0200

    Add token for sonar cloud

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  SONAR_TOKEN: 'e597eff9bf185118ed269cda4d0cab4f59499a56',
 };
 
 /*
