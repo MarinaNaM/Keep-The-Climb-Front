a799f475e3ce1bf93920f7906377781e758d3ae3
Marina <marinadlqs@gmail.com>
2022-07-19 10:20:45 +0200

    Add test profile page

--- src/app/profile/profile-page/profile-page.component.spec.ts
+++ src/app/profile/profile-page/profile-page.component.spec.ts
@@ -1,16 +1,52 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { AppState } from 'src/app/store/app.state';
 
 import { ProfilePageComponent } from './profile-page.component';
+
+const mockInitialState: AppState = {
+  schools: { schools: [] },
+  sectors: { sectors: [] },
+  routes: { routes: [] },
+  user: {
+    user: {
+      _id: '123456',
+      name: 'test-user',
+      psw: '123456789',
+      img: 'src',
+      email: 'usertest@gmail.com',
+      address: {
+        community: '',
+        province: '',
+      },
+      role: 'user',
+      routes: [
+        {
+          route: {
+            name: '',
+            length: 1,
+            grade: '',
+            voteGrade: [{ user: '', vote: 1 }],
+          },
+          isProject: false,
+          isEnchain: false,
+        },
+      ],
+    },
+    token: '',
+  },
+};
 
 describe('ProfilePageComponent', () => {
   let component: ProfilePageComponent;
   let fixture: ComponentFixture<ProfilePageComponent>;
+  let initialState = mockInitialState;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProfilePageComponent ]
-    })
-    .compileComponents();
+      declarations: [ProfilePageComponent],
+      providers: [provideMockStore({ initialState })],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProfilePageComponent);
     component = fixture.componentInstance;
