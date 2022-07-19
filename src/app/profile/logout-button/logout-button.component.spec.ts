bae84bf0700bd5698115db37c8cd3608a74c433c
Marina <marinadlqs@gmail.com>
2022-07-19 10:20:05 +0200

    Add test logout button

--- src/app/profile/logout-button/logout-button.component.spec.ts
+++ src/app/profile/logout-button/logout-button.component.spec.ts
@@ -1,16 +1,54 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { AppState } from 'src/app/store/app.state';
 
 import { LogoutButtonComponent } from './logout-button.component';
+
+const mockInitialState: AppState = {
+  schools: { schools: [] },
+  sectors: { sectors: [] },
+  routes: { routes: [] },
+  user: {
+    user: {
+      _id: '123456',
+      name: 'test-user',
+      psw: '123456789',
+      img: 'src',
+      email: 'usertest@gmail.com',
+      address: {
+        community: '',
+        province: '',
+      },
+      role: 'user',
+      routes: [
+        {
+          route: {
+            name: '',
+            length: 1,
+            grade: '',
+            voteGrade: [{ user: '123456', vote: 1 }],
+          },
+          isProject: false,
+          isEnchain: true,
+        },
+      ],
+    },
+    token: '',
+  },
+};
 
 describe('LogoutButtonComponent', () => {
   let component: LogoutButtonComponent;
   let fixture: ComponentFixture<LogoutButtonComponent>;
+  let initialState = mockInitialState;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LogoutButtonComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule],
+      declarations: [LogoutButtonComponent],
+      providers: [provideMockStore({ initialState })],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LogoutButtonComponent);
     component = fixture.componentInstance;
@@ -19,5 +57,21 @@ describe('LogoutButtonComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('When handleLogout is called', () => {
+    it('should call dispatch, localStorage and navigate', () => {
+      spyOn(component.store, 'dispatch');
+      spyOn(component.localStorage, 'clearToken');
+      spyOn(component.routerTo, 'navigate');
+
+      fixture.detectChanges();
+
+      component.handleLogout();
+
+      expect(component.store.dispatch).toHaveBeenCalled();
+      expect(component.localStorage.clearToken).toHaveBeenCalled();
+      expect(component.routerTo.navigate).toHaveBeenCalled();
+    });
   });
 });
